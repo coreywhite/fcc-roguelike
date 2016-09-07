c560cf0487ad0c53cac1d1c2a1d0900006456cac
Corey White <white.corey.a@gmail.com>
2016-09-07 07:12:55 -0400

    Start of key handling

--- src/GameView.js
+++ src/GameView.js
@@ -4,6 +4,16 @@ import './GameView.css';
 
 
 class GameView extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
   render() {
     return (
       <div className="game">
@@ -12,6 +22,25 @@ class GameView extends Component {
         <h2>Controls would go here</h2>
       </div>
     );
+  }
+  handleKeyPress(e) {
+    switch(e.keyCode) {
+      case 37:
+        //left
+        break;
+      case 38:
+        //up
+        break;
+      case 39:
+        //right
+        break;
+      case 40:
+        //down
+        break;
+      default:
+        break;
+    }
+    console.log(e.keyCode);
   }
 }
 
